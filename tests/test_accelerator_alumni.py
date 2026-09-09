e917f024555d6b9c1aa7e7b87b9531dadf6fc02e
ssashkann <aleksandrnikolenko52@gmail.com>
2026-09-09 12:55:34 +0300

    test: scope closure notification assertion

--- tests/test_accelerator_alumni.py
+++ tests/test_accelerator_alumni.py
@@ -208,7 +208,8 @@ async def test_explicit_cohort_closure_snapshots_and_opt_in_alumni_privacy():
             AcceleratorResidentSnapshot.membership_id.in_(membership_ids)
         ))).scalar_one() == 2
         assert (await db.execute(select(func.count(AcceleratorNotificationOutbox.id)).where(
-            AcceleratorNotificationOutbox.idempotency_key.like("cohort-completed:%")
+            AcceleratorNotificationOutbox.idempotency_key.like("cohort-completed:%"),
+            AcceleratorNotificationOutbox.membership_id.in_(membership_ids),
         ))).scalar_one() == 2
 
         own_snapshot = await get_membership_closure_snapshot(membership_ids[0], first, db)
